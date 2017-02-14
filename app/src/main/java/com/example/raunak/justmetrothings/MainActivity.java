110253941e8158ad417b45860df3f1f4da487f19
Bryze <aseem2301@gmail.com>
2017-02-14 12:32:46 +0530

    Master Commit
    This is master commit.
    Don't mess with it without all team approval

---
+++ app/src/main/java/com/example/raunak/justmetrothings/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.raunak.justmetrothings;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView profile =(TextView)findViewById(R.id.profile);
+        TextView metro  = (TextView)findViewById(R.id.metro);
+        TextView social_media = (TextView)findViewById(R.id.social_media);
+
+        metro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,MetroActivity.class);
+                startActivity(i);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        social_media.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,SocialMediaActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
