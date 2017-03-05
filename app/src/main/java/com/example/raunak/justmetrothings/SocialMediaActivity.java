cf397a8f07ffe16625f59588f67eacfb72fbf30c
Bryze <aseem2301@gmail.com>
2017-03-05 17:11:35 +0530

    Intermediate push
    Added layout for feeds
    Check activity_social_media.xml for preview
    Functionality not achieved yet.

--- app/src/main/java/com/example/raunak/justmetrothings/SocialMediaActivity.java
+++ app/src/main/java/com/example/raunak/justmetrothings/SocialMediaActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87d07d84b875c8737ce83fd9d59f261e8b85517d189879c291a82a4604d8b121
-size 375
+oid sha256:ca65508496f7fe7f0cd96993cea155adfe08b226f498a97d7c7b52b7fe4b5603
+size 1737
