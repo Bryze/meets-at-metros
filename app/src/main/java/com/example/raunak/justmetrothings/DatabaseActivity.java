09a5a6e2e45fa3fbf5081b396dd8705181ed93cc
rrc1709 <raunak.choraria1709@gmail.com>
2017-03-05 17:54:59 +0530

    Intermediate Push 2
    Child added to Realtime Database Json file.
    Location fixed to "Delhi"

--- app/src/main/java/com/example/raunak/justmetrothings/DatabaseActivity.java
+++ app/src/main/java/com/example/raunak/justmetrothings/DatabaseActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da1a5ef30f65b44c944f38cbbeb581eef3c956e98aa7c14a304145bee3392770
-size 3011
+oid sha256:e862d417e9aa47ba8f50aab3edb5f196ead8cb76376a153a02ca0e75ce491b89
+size 3032
--- app/src/main/java/com/example/raunak/justmetrothings/InfoFragment.java
+++ app/src/main/java/com/example/raunak/justmetrothings/InfoFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54b2334073c1d47a54da8ddedf159398f7d341e1eecebc7e93dd5dfd9d2ebc35
-size 2524
+oid sha256:33620bd2f2e2a710962afd38f827d60037541334cc1d50ca7eacdee4e92a3a50
+size 2545
