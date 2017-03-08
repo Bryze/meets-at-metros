6769a28ea909502f2bafccd959468ad867113f55
rrc1709 <raunak.choraria1709@gmail.com>
2017-03-08 14:34:11 +0530

    Feeds Added
    Feeds Storage and retreival

---
+++ app/src/main/java/com/example/raunak/justmetrothings/FeedsClass.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c69eb1ab43c4012ebaec4e43735d8c927055b5c4a40bc115419b0deac3bca213
+size 398
--- app/src/main/java/com/example/raunak/justmetrothings/SocialMediaActivity.java
+++ app/src/main/java/com/example/raunak/justmetrothings/SocialMediaActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca65508496f7fe7f0cd96993cea155adfe08b226f498a97d7c7b52b7fe4b5603
-size 1737
+oid sha256:b258a481cc60f3d44422615b276a692972016067fcf468350fda519b87949cb3
+size 3732
