66a7e674a6f63009d81d4e4b4af15037734e3bbf
rrc1709 <raunak.choraria1709@gmail.com>
2017-03-22 10:56:08 +0530

    Fragments - confirmation commit

--- app/src/main/java/com/example/raunak/justmetrothings/SocialMediaActivity.java
+++ app/src/main/java/com/example/raunak/justmetrothings/SocialMediaActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db9946b21a9390528eeca4f31d53ef17d745d9e788fd2c7a6466dfa5374f8210
-size 1984
+oid sha256:53b849354b234723ae8b13fb963c7ea8d8dbd46fd0e327e6028c4b65b088fac8
+size 1989
