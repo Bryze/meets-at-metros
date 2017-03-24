1df28056939304cb65d20d149c3f1a16225e941f
rrc1709 <raunak.choraria1709@gmail.com>
2017-03-24 20:12:33 +0530

    Fix -  Issue  #13

--- app/src/main/java/com/example/raunak/justmetrothings/PublicFragment.java
+++ app/src/main/java/com/example/raunak/justmetrothings/PublicFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:352cf7ea184857729b0ffc8bf7eaaeadf5e71ffaa14978ef8382016e3dd185bc
-size 9210
+oid sha256:4c406530793c7cbe4e33e9e999b6aaf421a10aaba5be098532e889bbd9b0715b
+size 9571
