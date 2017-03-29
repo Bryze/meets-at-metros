e2d10149ec69c8b007402e5833d5eccaa72cc053
Bryze <aseem2301@gmail.com>
2017-03-30 01:03:13 +0530

    Fixed Personal Feeds for Profile
    Fixed personal feeds for profile. Users can now see their own posts in
    the logs section.

--- app/src/main/java/com/example/raunak/justmetrothings/LogsFragment.java
+++ app/src/main/java/com/example/raunak/justmetrothings/LogsFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c05ba38763845f2c86ccf080ce7836bbcbb4d196fc2a268cf384615949f09549
-size 3862
+oid sha256:ec1d008b49738bf5fe6fd6393766e81cdc3d0edc9aaf1c362974f352a69e1c9e
+size 4011
--- app/src/main/java/com/example/raunak/justmetrothings/PublicFragment.java
+++ app/src/main/java/com/example/raunak/justmetrothings/PublicFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54c858966066bb2daf89fb5f97a4660132a11535938f1c54b7e744c18f5637f7
-size 9665
+oid sha256:607d744a5cfe17324c14b87faee3835181d507abaf28eb569e91e1b4ec793ea2
+size 9885
