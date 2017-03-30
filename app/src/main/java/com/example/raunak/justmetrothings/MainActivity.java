d230f0a2c0ad15fff1a2cbe1c03691b43ea4527f
rrc1709 <raunak.choraria1709@gmail.com>
2017-03-30 17:17:51 +0530

    App Crashing Solved

--- app/src/main/java/com/example/raunak/justmetrothings/MainActivity.java
+++ app/src/main/java/com/example/raunak/justmetrothings/MainActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2edf949b4b48560d6b8626a33c8f74e817366a9dd403d5b54b4df6a048857d6f
-size 2765
+oid sha256:dd0b86fc8a276f18a34e43b071097445308d0a4b91d739aad5052a06be47cf95
+size 2811
