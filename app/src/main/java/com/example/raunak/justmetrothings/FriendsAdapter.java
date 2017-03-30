c735e96ab93049ca364515077bd80426fc6c5cce
Shantanu <shantanu.pandey9@yahoo.com>
2017-03-30 22:54:51 +0530

    Added Adapter related Changes
    The adapter can't push just yet.
    Image retrieving and pushing has been avoided by since, i have no idea
    how that works.
    Tried implementing dummy, but i was facing errors and thus decided to
    leave that out.

---
+++ app/src/main/java/com/example/raunak/justmetrothings/FriendsAdapter.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e39495f2b7080f07a6c279a10b8e71f3da1f69c22ea37e616b9fe47c50dcbc7
+size 2080
--- app/src/main/java/com/example/raunak/justmetrothings/FriendsFragment.java
+++ app/src/main/java/com/example/raunak/justmetrothings/FriendsFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0753f2bb505c7f44d8bfdb4d1faff11c8a632ddafa18e0175f2e75b7502f9cc7
-size 699
+oid sha256:a89e2c55a4c11ab0501f3058e478d055d2ca8c65b526c19fd397d41d1d5b45c4
+size 1789
