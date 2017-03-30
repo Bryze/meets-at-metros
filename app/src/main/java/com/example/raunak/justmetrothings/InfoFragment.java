5a83c3bcaa32b230d00b2bf7fdaef3a745bf2f6a
rrc1709 <raunak.choraria1709@gmail.com>
2017-03-30 16:08:53 +0530

    Changes

--- app/src/main/java/com/example/raunak/justmetrothings/InfoFragment.java
+++ app/src/main/java/com/example/raunak/justmetrothings/InfoFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70f89d0be4359643825c9fdf6297ab9783d27e2b38c8ebb40d99a92d40787e5f
-size 7116
+oid sha256:96013810f6ace356e685bbf95e378045a5b74b9e11a4691f68e16bc3068249ee
+size 7422
