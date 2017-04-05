91be7d3148cecef5b0396d4bd4c3a7e6c09f93bd
rrc1709 <raunak.choraria1709@gmail.com>
2017-04-06 01:22:37 +0530

    Placeholder Image added

--- app/src/main/java/com/example/raunak/justmetrothings/DatabaseActivity.java
+++ app/src/main/java/com/example/raunak/justmetrothings/DatabaseActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb5fd0553064a39f23d70475b3e34d6d1ced870bb03877ee311970b09f7ac0f6
-size 5772
+oid sha256:80783e62c8b7ffb6deaa95397a43a3e68f42a3683a2ed9e86c3a5b7770eee6c5
+size 5932
--- app/src/main/java/com/example/raunak/justmetrothings/FriendsFragment.java
+++ app/src/main/java/com/example/raunak/justmetrothings/FriendsFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5e982b932bda2d2e2718fb7cde2256423a10af96a0e3d835ae80fce171bbaad
-size 5604
+oid sha256:537dca6bef526a17fc3dbb9e227e06a53eb19b647862de65a8c8f608a569346c
+size 4923
