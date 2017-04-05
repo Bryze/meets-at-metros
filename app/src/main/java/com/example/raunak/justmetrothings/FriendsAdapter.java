9b2665df23bce2c8fb41d0db966dc256fb44d67f
rrc1709 <raunak.choraria1709@gmail.com>
2017-04-05 17:25:08 +0530

    File Deletions

--- app/src/main/java/com/example/raunak/justmetrothings/FriendsAdapter.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d55a8c6b9a78e9c5be2b46698b4f8f854e0404c413ac90940abaf86497bdd8fa
-size 2083
--- app/src/main/java/com/example/raunak/justmetrothings/FriendsFragment.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f706e417b3d8d0798cf2e620e8ab624fa251779e9894ad49d8c24e9a1bff5026
-size 4380
